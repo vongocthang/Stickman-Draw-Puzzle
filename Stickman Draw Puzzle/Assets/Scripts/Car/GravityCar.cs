194188cfd4af2ef9741eb25de3cb33789bfe4d6b
Võ Ngọc Thắng <vongocthang160199@gmail.com>
2021-11-11 17:37:55 +0700

    a

--- Stickman Draw Puzzle/Assets/Scripts/Car/GravityCar.cs
+++ Stickman Draw Puzzle/Assets/Scripts/Car/GravityCar.cs
@@ -6,14 +6,6 @@ using Spine.Unity;
 
 public class GravityCar : MonoBehaviour
 {
-    UIControl uiControl;
-
-    public Rigidbody2D thisRb;
-    public int moveSpeed;
-    public int tempMoveSpeed;
-
-    public TargetJoint2D targetJoint;
-
     public GameObject forwardWheel;
     public GameObject backWheel;
 
@@ -21,27 +13,13 @@ public class GravityCar : MonoBehaviour
     public int countAnim = 1;
     public float countTime;
 
-    public float tempRotation;
-
+    FixedJoystick joy;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        uiControl = GameObject.Find("MainUI").GetComponent<UIControl>();
-
-        if (this.name == "CarLeft")
-        {
-            tempRotation = 90;
-            forwardWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-            backWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-        }
-        else
-        {
-            tempRotation = -90;
-            forwardWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-            backWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-        }
+        joy = GameObject.FindObjectOfType<FixedJoystick>();
     }
 
     // Update is called once per frame
@@ -52,130 +30,35 @@ public class GravityCar : MonoBehaviour
 
     public void Move()
     {
-        if (uiControl.moveLeft == true)
+        Vector2 a = new Vector2(0, 0);
+        Vector2 b = joy.Direction;
+        float canhHuyen = Vector2.Distance(a, b);
+        float canhKe = a.y - b.y;
+        float goc = (float)(canhKe / canhHuyen) * Mathf.Rad2Deg;
+
+        //forwardWheel.transform.rotation = Quaternion.Euler(0, 0, goc);
+ 
+        Debug.Log(goc);
+
+        //Animation
+        if (joy.Direction.x <= 0)
         {
-            targetJoint.enabled = false;
-            //Animation
-            countAnim = 1;
-            if (this.name == "CarLeft")
-            {
-                skeletonAnim.AnimationName = "Forward";
-                //Xoay động cơ
-                tempRotation = Mathf.MoveTowards(tempRotation, 180, 10);
-                forwardWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-                backWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-            }
-            else
-            {
-                skeletonAnim.AnimationName = "Backward";
-                //Xoay động cơ
-                tempRotation = Mathf.MoveTowards(tempRotation, 0, 10);
-                forwardWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-                backWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-            }
-            //Tốc độ tăng dần
-            if (Mathf.Abs(tempMoveSpeed) < moveSpeed)
-            {
-                tempMoveSpeed -= 3;
-                thisRb.velocity = new Vector2(tempMoveSpeed * Time.deltaTime, 0f);
-            } 
-        }
-
-        if (uiControl.moveRight == true)
-        {
-            targetJoint.enabled = false;
-            //Animation
-            countAnim = 1;
-            if (this.name == "CarLeft")
-            {
-                skeletonAnim.AnimationName = "Backward";
-                //Xoay động cơ
-                tempRotation = Mathf.MoveTowards(tempRotation, 0, 10);
-                forwardWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-                backWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-
-            }
-            else
-            {
-                skeletonAnim.AnimationName = "Forward";
-                //Xoay động cơ
-                tempRotation = Mathf.MoveTowards(tempRotation, -180, 10);
-                forwardWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-                backWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-
-            }
-            //Tốc độ tăng dần
-            if (Mathf.Abs(tempMoveSpeed) < moveSpeed)
-            {
-                tempMoveSpeed += 3;
-                thisRb.velocity = new Vector2(tempMoveSpeed * Time.deltaTime, 0f);
-            }
-        }
-
-        if (uiControl.moveUp == true)
-        {
-            targetJoint.enabled = false;
-            countAnim = 1;
             skeletonAnim.AnimationName = "Forward";
-            //Tốc độ tăng dần
-            if (Mathf.Abs(tempMoveSpeed) < moveSpeed)
-            {
-                tempMoveSpeed += 3;
-                thisRb.velocity = new Vector2(0f, tempMoveSpeed * Time.deltaTime);
-            }
-            if (this.name == "CarLeft")
-            {
-                //Xoay động cơ
-                tempRotation = Mathf.MoveTowards(tempRotation, 90, 10);
-                forwardWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-                backWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-
-            }
-            else
-            {
-                //Xoay động cơ
-                tempRotation = Mathf.MoveTowards(tempRotation, -90, 10);
-                forwardWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-                backWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-
-            }
+            goc += 180;
+            forwardWheel.transform.rotation = Quaternion.Euler(0, 0, goc);
         }
-
-        if (uiControl.moveDown == true)
+        else
         {
-            targetJoint.enabled = false;
-            countAnim = 1;
-            skeletonAnim.AnimationName = "Forward";
-            //Tốc độ tăng dần
-            if (Mathf.Abs(tempMoveSpeed) < moveSpeed)
-            {
-                tempMoveSpeed -= 3;
-                thisRb.velocity = new Vector2(0f, tempMoveSpeed * Time.deltaTime);
-            }
-            if (this.name == "CarLeft")
-            {
-                //Xoay động cơ
-                tempRotation = Mathf.MoveTowards(tempRotation, -90, 10);
-                forwardWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-                backWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-
-            }
-            else
-            {
-                //Xoay động cơ
-                tempRotation = Mathf.MoveTowards(tempRotation, 90, 10);
-                forwardWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-                backWheel.transform.rotation = Quaternion.Euler(0, 0, tempRotation);
-
-            }
+            skeletonAnim.AnimationName = "Backward";
+            //goc -= 30;
+            //goc -= 180;
+            forwardWheel.transform.rotation = Quaternion.Euler(0, 0, -goc);
         }
+        
 
-        if (uiControl.moveLeft == false && uiControl.moveRight == false && uiControl.moveUp == false &&
-            uiControl.moveDown == false)
+
+        if (joy.Direction == new Vector2(0,0))
         {
-            targetJoint.enabled = true;
-            tempMoveSpeed = 0;
-
             //Animation
             if (Time.time < countTime + 5)
             {
